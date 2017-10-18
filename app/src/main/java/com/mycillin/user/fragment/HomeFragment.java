572f8f256052ad26427970752d120215318f3537
Mahardhika Rizki Bagaskoro <mahardhika.bagaskoro@muf.co.id>
2017-10-18 10:50:48 +0700

    Add get account list on Home Fragment

--- app/src/main/java/com/mycillin/user/fragment/HomeFragment.java
+++ app/src/main/java/com/mycillin/user/fragment/HomeFragment.java
@@ -2,7 +2,11 @@ package com.mycillin.user.fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,17 +15,36 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.mycillin.user.R;
+import com.mycillin.user.activity.AccountActivity;
+import com.mycillin.user.activity.AccountDetailActivity;
 import com.mycillin.user.activity.ServiceBookDoctorActivity;
 import com.mycillin.user.activity.ServiceMedicalReservationActivity;
+import com.mycillin.user.adapter.AccountAdapter;
+import com.mycillin.user.list.AccountList;
+import com.mycillin.user.rest.MyCillinAPI;
+import com.mycillin.user.rest.MyCillinRestClient;
+import com.mycillin.user.rest.accountList.ModelResultAccountList;
+import com.mycillin.user.util.ProgressBarHandler;
+import com.mycillin.user.util.RecyclerTouchListener;
+import com.mycillin.user.util.SessionManager;
+import com.orhanobut.dialogplus.DialogPlus;
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageListener;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import in.galaxyofandroid.spinerdialog.OnSpinerItemClick;
 import in.galaxyofandroid.spinerdialog.SpinnerDialog;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class HomeFragment extends Fragment {
 
@@ -42,6 +65,7 @@ public class HomeFragment extends Fragment {
     LinearLayout goToRedeemPrescriptionService;
 
     private ArrayList<String> items = new ArrayList<>();
+    private ProgressBarHandler progressBarHandler;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -58,12 +82,14 @@ public class HomeFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
         ButterKnife.bind(this, rootView);
 
+        progressBarHandler = new ProgressBarHandler(getContext());
+
         final SpinnerDialog spinnerDialog = new SpinnerDialog(getActivity(), items, getString(R.string.servicesActivity_dropdownTitle), R.style.DialogAnimations_SmileWindow);
 
-        items.add("Me");
+        /*items.add("Me");
         items.add("Wife");
         items.add("Son");
-        items.add("Daughter");
+        items.add("Daughter");*/
 
         spinnerDialog.bindOnSpinerListener(new OnSpinerItemClick() {
             @Override
@@ -75,7 +101,7 @@ public class HomeFragment extends Fragment {
         tvDropdwon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                spinnerDialog.showSpinerDialog();
+                getAccountList(spinnerDialog);
             }
         });
 
@@ -106,5 +132,73 @@ public class HomeFragment extends Fragment {
 
 
         return rootView;
+    }
+
+    private void getAccountList(final SpinnerDialog spinnerDialog) {
+        progressBarHandler.show();
+
+        SessionManager sessionManager = new SessionManager(getContext());
+        String token = sessionManager.getUserToken();
+        String userId = sessionManager.getUserId();
+
+        MyCillinAPI myCillinAPI = MyCillinRestClient.getMyCillinRestInterface();
+
+        HashMap<String, String> params = new HashMap<>();
+        params.put("user_id", userId);
+
+        myCillinAPI.getAccountList(token, params).enqueue(new Callback<ModelResultAccountList>() {
+            @Override
+            public void onResponse(@NonNull Call<ModelResultAccountList> call, @NonNull Response<ModelResultAccountList> response) {
+                progressBarHandler.hide();
+
+                if(response.isSuccessful()) {
+                    ModelResultAccountList modelResultAccountList = response.body();
+
+                    assert modelResultAccountList != null;
+                    if(modelResultAccountList.getResult().isStatus()) {
+
+                        int size = modelResultAccountList.getResult().getData().size();
+                        for(int i = 0; i < size; i++) {
+                            String accountPic = "pic_01.jpg";
+                            String accountName = modelResultAccountList.getResult().getData().get(i).getFullName();
+                            String accountCreatedBy = modelResultAccountList.getResult().getData().get(i).getCreatedBy();
+                            String accountCreatedDate = modelResultAccountList.getResult().getData().get(i).getCreatedDate();
+                            String accountUpdatedBy = modelResultAccountList.getResult().getData().get(i).getUpdatedBy();
+                            String accountUpdatedDate = modelResultAccountList.getResult().getData().get(i).getUpdatedDate();
+                            String accountRelationId = modelResultAccountList.getResult().getData().get(i).getRelationId();
+                            String accountRelationType = modelResultAccountList.getResult().getData().get(i).getRelationType();
+                            String accountUserId = modelResultAccountList.getResult().getData().get(i).getUserId();
+                            String accountGender = modelResultAccountList.getResult().getData().get(i).getGender();
+                            String accountAddress = modelResultAccountList.getResult().getData().get(i).getAddress();
+                            String accountMobileNo = modelResultAccountList.getResult().getData().get(i).getMobileNo();
+                            String accountDOB = modelResultAccountList.getResult().getData().get(i).getDob();
+                            String accountHeight = modelResultAccountList.getResult().getData().get(i).getHeight();
+                            String accountWeight = modelResultAccountList.getResult().getData().get(i).getWeight();
+                            String accountBloodType = modelResultAccountList.getResult().getData().get(i).getBloodType();
+                            String accountInsuranceId = modelResultAccountList.getResult().getData().get(i).getInsuranceId();
+
+                            items.add(accountName);
+                        }
+                        spinnerDialog.showSpinerDialog();
+                    }
+                }
+                else {
+                    try {
+                        JSONObject jsonObject = new JSONObject(response.errorBody().string());
+                        String message = jsonObject.getJSONObject("result").getString("message");
+                        Snackbar.make(getActivity().getWindow().getDecorView().getRootView(), message, Snackbar.LENGTH_SHORT).show();
+                    } catch (JSONException | IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<ModelResultAccountList> call, @NonNull Throwable t) {
+                // TODO: 12/10/2017 SET FAILURE SCENARIO
+                progressBarHandler.hide();
+                Snackbar.make(getActivity().getWindow().getDecorView().getRootView(), t.getMessage(), Snackbar.LENGTH_SHORT).show();
+            }
+        });
     }
 }
