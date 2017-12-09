d3b24d4455db6b2c69444fa0830b48a2abe91dd1
Mahardhika Bagaskoro <mrbagaskoro@gmail.com>
2017-12-09 16:55:43 +0700

    Add onBackPressed to MainActivity on PINActivity

--- app/src/main/java/com/mycillin/user/activity/PINActivity.java
+++ app/src/main/java/com/mycillin/user/activity/PINActivity.java
@@ -1,10 +1,10 @@
 package com.mycillin.user.activity;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -408,7 +408,11 @@ public class PINActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onBackPressed() {}
+    public void onBackPressed() {
+        Intent intent = new Intent(PINActivity.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
 
     private void accessGranted() {
         finish();
